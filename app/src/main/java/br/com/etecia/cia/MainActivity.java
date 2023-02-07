102f4cab3ec0972edfe583af2d8155ac88cec5dd
AlisonDuarte <dru.rofrigo@hotmail.com>
2023-02-07 20:27:28 -0300

    Inicio

--- app/src/main/java/br/com/etecia/cia/MainActivity.java
+++ app/src/main/java/br/com/etecia/cia/MainActivity.java
@@ -3,8 +3,13 @@ package br.com.etecia.cia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView idListaValores;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
