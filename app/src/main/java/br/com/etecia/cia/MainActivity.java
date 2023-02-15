70ab33cbc441fe5c621f7be54fc07b4c7c38e787
AlisonDuarte <dru.rofrigo@hotmail.com>
2023-02-14 21:35:07 -0300

    Finalização

--- app/src/main/java/br/com/etecia/cia/MainActivity.java
+++ app/src/main/java/br/com/etecia/cia/MainActivity.java
@@ -6,11 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     ListView idListaValores;
+
+    //CRIAR A BASE DE DADOS PARA OS FILMES
+    String titulo[] = {"Rafiting", "Polo", "Caiaque", "Surf", "Windsurf"};
+    String descricao[] = {"Aqui é a descrição do esporte, onde temos que enrolar muito para saber o tamanho do espaço.",
+            "Aqui é a descrição do esporte, onde temos que enrolar muito para saber o tamanho do espaço.",
+            "A nova invenção de Van Helsing transforma Drac e os amigos em humanos, e Johnny em um monstro. Agora, Drac deve encontrar uma maneira de reverter o feitiço antes que a mudança se torne permanente.",
+            "Os valentes pinguins Capitão, Kowalski, Rico e Recruta se unem a uma força de elite, chamada Vento do Norte, para impedir que o vilão Dr. Octavius Brine acabe com os pinguins do mundo todo.",
+            "Na glamourosa cidade de Redshore, Buster Moon e a galera enfrentam seus medos, fazem novos amigos e superam seus limites em uma jornada para convencer o recluso astro Clay Calloway a subir aos palcos novamente."
+    };
+    String avaliacao[] = {"4.5", "4.0", "5.0", "2.0", "3.7"};
+    int imagem[] = {R.drawable.rafiting, R.drawable.polo, R.drawable.caiaque, R.drawable.surf, R.drawable.windsurf};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +32,6 @@ public class MainActivity extends AppCompatActivity {
         idListaValores = findViewById(R.id.idListaValores);
         MyAdapter adapter = new MyAdapter();
         idListaValores.setAdapter(adapter);
-
-
     }
 
 
@@ -44,7 +55,26 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            return null;
+            ImageView idimagem;
+            TextView idtitulo, iddescricao, idavaliacao;
+
+            //ADAPTADOR LIGANDO AO MODELO
+
+            View v = getLayoutInflater().inflate(R.layout.modelo_juntos, null);
+
+            //APRESENTANDO AS VARIAVEIS DO JAVA PARA O XML
+            imagem = v.findViewById(R.id.idimagem);
+            titulo = v.findViewById(R.id.idtitulo);
+            descricao = v.findViewById(R.id.iddescricao);
+            avaliacao = v.findViewById(R.id.idavaliacao);
+
+
+            //inserindo valores nas variaveis
+            titulo.setText(titulo[i]);
+            descricao.setText(descricao[i]);
+            imagem.setImageResource(imagem[i]);
+            avaliacao.setText(avaliacao[i]);
+            return v;
         }
     }
 
